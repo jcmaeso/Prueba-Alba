ed9cfe5255fd852b0ed5445cd1e8dd7d1e1a9b2b
Jorge Calatayud <jorge.cmaeso@alumnos.upm.es>
2018-04-27 14:44:51 +0200

    Version 1.0

--- i2c_controller.h
+++ i2c_controller.h
@@ -8,7 +8,7 @@
 #ifndef I2C_CONTROLLER_H_
 #define I2C_CONTROLLER_H_
 
-void I2C_setUp(unsigned int cpuInt,unsigned int slave_addr);
+void I2C_setUpPENE(unsigned int cpuInt,unsigned int slave_addr);
 unsigned char I2C_transmit(unsigned int slave_addr,int data_length, unsigned char* data);
 void I2C_receive(unsigned int slave_addr, int data_out_length, unsigned char* data,unsigned char* rx);
 void I2C_test();
